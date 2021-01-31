7fe2693b3e31daf5e7a215f20b9663f0892a90fd
zawette <zawette@gmail.com>
2021-01-31 17:16:45 +0100

    tests init

---
+++ Tests/Unit/CreateItemTests.cs
@@ -0,0 +1,32 @@
+using System.ComponentModel;
+using System;
+using Xunit;
+using Domain.Entities;
+using Shouldly;
+using System.Linq;
+using Domain.Events;
+
+namespace Tests.Unit
+{
+    public class CreateItemTests
+    {
+        [Fact]
+        public void given_valid_id_item_should_be_created()
+        {   
+            //Arange
+            var id= new Guid();
+            var amount = 1;
+            
+            //Act
+            var item = Item.Create(id,amount);
+            
+            //Assert
+            item.ShouldNotBeNull(); 
+            item.Id.ShouldBe(id);
+            item.Events.Count.ShouldBe(1);
+            
+            var @event = item.Events.Single();
+            @event.ShouldBeOfType<ItemCreated>();
+        }
+    }
+}
